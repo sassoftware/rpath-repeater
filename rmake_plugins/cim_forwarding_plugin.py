7153f9855f375d979c75d1f4c94daf716ecfc052
Ben Rector <https://issues.rpath.com/>
2010-08-05 11:06:34 -0400

    adding CIM repeater skeleton

---
+++ rmake_plugins/cim_forwarding_plugin.py
@@ -0,0 +1,87 @@
+#
+# Copyright (c) 2010 rPath, Inc.
+#
+# This program is distributed under the terms of the Common Public License,
+# version 1.0. A copy of this license should have been distributed with this
+# source file in a file called LICENSE. If it is not present, the license
+# is always available at http://www.rpath.com/permanent/licenses/CPL-1.0.
+#
+# This program is distributed in the hope that it will be useful, but
+# without any warranty; without even the implied warranty of merchantability
+# or fitness for a particular purpose. See the Common Public License for
+# full details.
+#
+
+from rmake.core import handler
+from rmake.core import plug_dispatcher
+from rmake.core import types
+from rmake.worker import plug_worker
+
+
+PREFIX = 'com.rpath.sputnik.cimplugin'
+CIM_JOB = PREFIX
+CIM_TASK_RACTIVATE = PREFIX + '.ractivate'
+CIM_TASK_POLLING = PREFIX + '.poll'
+
+
+class CimForwardingPlugin(plug_dispatcher.DispatcherPlugin, plug_worker.WorkerPlugin):
+    
+    def dispatcher_pre_setup(self, dispatcher):
+        handler.registerHandler(CimHandler)
+
+    def worker_get_task_types(self):
+        return {
+                CIM_TASK_RACTIVATE: RactivateTask,
+                CIM_TASK_POLLING: PollingTask,
+                }
+        
+class CimHandler(handler.JobHandler):
+    
+    def ractivate(self, host):
+        params = CimParams(self.cert, self.key)
+        task = self.newTask('rActivate', CIM_TASK_RACTIVATE,
+                RactivateData(params, host, self.port))
+        def cb_gather(results):
+            return 'done'
+        return self.gatherTasks([task], cb_gather)
+    
+    def polling(self, host):
+        params = CimParams(self.cert, self.key)
+        task = self.newTask('Polling', CIM_TASK_POLLING,
+                PollingData(params, host, self.port))
+        def cb_gather(results):
+            return 'done'
+        return self.gatherTasks([task], cb_gather)    
+    
+CimParams = types.slottype('CimParams', 'cert key')
+# These are just the starting point attributes
+RactivateData = types.slottype('RactivateData', 'p host port response')
+PollingData = types.slottype('PollingData', 'p host port response')
+    
+class RactivateTask(plug_worker.TaskHandler):
+
+    def run(self):
+        data = self.getData()
+        self.sendStatus(101, "Contacting host %s on port %d to rActivate itself" % (
+            data.host, data.port))
+
+        #send CIM rActivate request
+        data.response = ""
+
+        self.setData(data)
+        self.sendStatus(200, "Host %s will try to rActivate itself" % data.host)
+
+
+class PollingTask(plug_worker.TaskHandler):
+
+    def run(self):
+        data = self.getData()
+        self.sendStatus(101, "Contacting host %s on port %d to Poll it for info" % (
+            data.host, data.port))
+
+        #send CIM poll request
+        data.response = ""
+
+        self.setData(data)
+        self.sendStatus(200, "Host %s has been polled" % data.host)
+    
