c88b14b7b02cb72f69a04116a25d5ed495bb4757
Ben Rector <https://issues.rpath.com/>
2010-08-09 23:36:04 -0400

    fixes after testing with the rmake client

--- rmake_plugins/cim_forwarding_plugin.py
+++ rmake_plugins/cim_forwarding_plugin.py
@@ -37,28 +37,47 @@ class CimForwardingPlugin(plug_dispatcher.DispatcherPlugin, plug_worker.WorkerPl
         
 class CimHandler(handler.JobHandler):
     
-    def ractivate(self, host):
-        params = CimParams(self.cert, self.key)
+    timeout = 3600
+    port = 5989
+        
+    jobType = CIM_JOB
+    firstState = 'ractivate'
+       
+    def ractivate(self):
+        self.setStatus(102, "Starting the rActivation {1/2}")
+        params = CimParams(self.timeout)
+        data = eval(self.getData())
         task = self.newTask('rActivate', CIM_TASK_RACTIVATE,
-                RactivateData(params, host, self.port))
+                RactivateData(params, data['host'], self.port))
         def cb_gather(results):
+            task, = results
+            result = task.task_data.getObject().response
+            self.job.data = types.FrozenObject.fromObject(result)
+            self.setStatus(200, "Done! cim ractivation response: %s" % (result))
             return 'done'
         return self.gatherTasks([task], cb_gather)
     
-    def polling(self, host):
+    def polling(self):
+        self.setStatus(102, "Starting the polling {1/2}")
         params = CimParams(self.cert, self.key)
         task = self.newTask('Polling', CIM_TASK_POLLING,
                 PollingData(params, host, self.port))
         def cb_gather(results):
+            task, = results
+            result = task.task_data.getObject().response
+            self.job.data = types.FrozenObject.fromObject(result)
+            self.setStatus(200, "Done! cim polling of %s" % (result))
             return 'done'
         return self.gatherTasks([task], cb_gather)    
     
-CimParams = types.slottype('CimParams', 'cert key')
+CimParams = types.slottype('CimParams', 'timeout')
 # These are just the starting point attributes
 RactivateData = types.slottype('RactivateData', 'p host port response')
 PollingData = types.slottype('PollingData', 'p host port response')
     
 class RactivateTask(plug_worker.TaskHandler):
+    
+    
 
     def run(self):
         data = self.getData()
@@ -66,7 +85,7 @@ class RactivateTask(plug_worker.TaskHandler):
             data.host, data.port))
 
         #send CIM rActivate request
-        data.response = ""
+        data.response = "<system/>"
 
         self.setData(data)
         self.sendStatus(200, "Host %s will try to rActivate itself" % data.host)
