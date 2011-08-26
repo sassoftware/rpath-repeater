f0e8999ad139a8da8d3762ee1d318f23928edd9c
Michael Tharp <mtharp@rpath.com>
2011-08-26 13:35:59 +0000

    Fix test after metadata changes

--- tests/modelstest.py
+++ tests/modelstest.py
@@ -16,9 +16,7 @@ class ModelsTest(TestBase):
             models.ImageFile(title="i1", sha1="s1", size=1),
             models.ImageFile(title="i2", sha1="s2"),
         ])
-        metadata = models.ImageMetadata(owner="me")
-        files.append(metadata)
         self.failUnlessEqual(files.toXml(),
-            '<files><file><title>i1</title><size>1</size><sha1>s1</sha1></file><file><title>i2</title><sha1>s2</sha1></file><metadata><owner>me</owner></metadata></files>')
+            '<files><file><title>i1</title><size>1</size><sha1>s1</sha1></file><file><title>i2</title><sha1>s2</sha1></file></files>')
 
 testsuite.main()
