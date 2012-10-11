83c84de73797d66178abc6e950f01aa5e7e3d4c1
Michael Tharp <mtharp@rpath.com>
2012-10-11 16:50:02 -0400

    Remove 'sudo' from invocation of smbmount

--- rpath_repeater/utils/windows/smbclient.py
+++ rpath_repeater/utils/windows/smbclient.py
@@ -57,7 +57,7 @@ class SMBClient(object):
 
         # Older mount.cifs don't seem to support passing the user via an
         # environment variable
-        self._mount_cmd = [ 'sudo', '/bin/mount', '-n', '-t', 'cifs', '-o',
+        self._mount_cmd = [ '/bin/mount', '-n', '-t', 'cifs', '-o',
             'user=%(user)s,password=%(password)s,forcedirectio',
             '//%%(host)s/%s$' % self._driveLetter ]
         self._mount_env = dict(PASSWD=self._authInfo.password)
