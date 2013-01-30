4194159efad6c821851a6ee20da60f2a4216a7c7
Michael Tharp <michael.tharp@sas.com>
2013-01-30 15:38:52 +0000

    Relicense under Apache License version 2.0

--- tests/rmake_pluginstest/testtaskhandler_assimilator.py
+++ tests/rmake_pluginstest/testtaskhandler_assimilator.py
@@ -1,7 +1,24 @@
-# Copyright (C) 2011 rPath, Inc.
 #
-# Mock tests for (Linux) Assimilation Plugin
-# does not actually SSH here, see scripts/demo_assimilate.py for that
+# Copyright (c) SAS Institute Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+Mock tests for (Linux) Assimilation Plugin
+does not actually SSH here, see scripts/demo_assimilate.py for that
+"""
+
 
 import testsuite
 testsuite.setup()
@@ -195,4 +212,3 @@ class AssimilatorTest(TestBase):
         #self.assertXMLEquals(taskData.object.response, expected_response)
 
 testsuite.main()
-
